907995089b774abf3f9999d24e5a48ee12ad0bf8
Khoerul Umam <40732987+plannerman@users.noreply.github.com>
2018-08-06 09:03:50 +0700

    Update kirim.php

--- kirim.php
+++ kirim.php
@@ -5,11 +5,14 @@ if(isset($_POST['submit'])){
 $dari = $_POST['dari'];
 $untuk = $_POST['untuk'];
 $isi = $_POST['isi'];
+  
+date_default_timezone_set('Asia/Jakarta');
+$now = date('d M Y , H:i')
 
-$sql = "INSERT INTO pesan (dari, untuk, isi) VALUES ('$dari', '$untuk', '$isi');";
+$sql = "INSERT INTO pesan (dari, untuk, isi, time) VALUES ('$dari', '$untuk', '$isi' , '$now');";
 mysqli_query($kon, $sql);
 
 header("Location: index.php?status=oke");
 }
 
-echo 'Hayo ngapain';
+echo 'Tidak memiliki akses!';
