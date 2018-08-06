d4e89b433de61246918ebfb51a1d8d7888e154cc
Khoerul Umam <40732987+plannerman@users.noreply.github.com>
2018-08-06 09:00:46 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,8 @@
 <?php
 
 $host = "localhost";
-$user = "root";
-$pass = "";
-$name = "saran";
+$user = "root";     //Username dari database
+$pass = "";         //Password untuk database
+$name = "saran";    //Nama database
 
 $kon = mysqli_connect($host, $user, $pass, $name);
-
-echo 'Hayo ngapain!';
